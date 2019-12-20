1eaca43bc9f003603681b3eb88756804c79168ac
Steven Barquet <sbarquet@interware.com.mx>
2019-12-20 13:27:43 -0600

    ajuste detalles home

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -12,7 +12,10 @@ const App = () => (
   <BrowserRouter>
         <Navbar />
         <Route exact path='/' component={Home}/>
-        <Route exact path='/tareas' component={Carrusel}/>    
+        <Route exact path='/tareas' component={Carrusel}/>
+        <a className="whats-icon" target="_blank" href="https://web.whatsapp.com/send?phone=5215533700670&amp;text=howard.servehttp.com:3000/%0A%0AHola,%20me%20gustaría%20obtener%20más%20información.">
+          <img src="https://cdn.shopify.com/s/files/1/0070/3666/5911/files/icon_only_new_inverted.png?469" alt="whatsapp" width="54px" />
+        </a>   
   </BrowserRouter>
 );
 
